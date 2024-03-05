a07576b783ac5517594f2246448c24629dcbe631
antoineruzette <antoine.ruzette@gmail.com>
2024-03-05 13:29:40 -0500

    Add logic to parse bib files and add them to README.md

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# _scienceclassics_
+# _science classics_
 Collection of classic papers in sciences, spanning biology, engineering and physics. 
 
 I read a lot of papers through my academic job - new and old sciences,spanning biology, engineering, computer science and physics. When keeping up with latest papers, I appreciate reading the old stuffs, reading the years of amazing sciences that led to this exact moment. Which papers led to the creation of entire field? To new ways of thinking? To paradigm shift? 
 
 To those who I find influential, inspirational and visionary, this repository is an attempt at archiving your research. 
 
--- 
+--
 
-1. 
+- Lazebnik, Yuri (2). Can a biologist fix a radio?--Or, what I learned while studying apoptosis. Cancer cell, 3(179--182), 2002.
--- source/extract_bibtex.py
+++ source/extract_bibtex.py
@@ -1,26 +1,69 @@
 import os
 import re
 
+
 def extract_papers_from_bibtex(bibtex_file):
     with open(bibtex_file, 'r') as file:
         bibtex_content = file.read()
-    pattern = re.compile(r'@article{(.*?),(.*?)\}', re.DOTALL)
-    papers = pattern.findall(bibtex_content)
+        
+        pattern = re.compile(r'@article{(.*?),\s*'
+                             r'title\s*=\s*{(?P<title>.*?)},\s*'
+                             r'author\s*=\s*{(?P<author>.*?)},\s*'
+                             r'journal\s*=\s*{(?P<journal>.*?)},\s*'
+                             r'volume\s*=\s*{(?P<volume>.*?)},\s*'
+                             r'number\s*=\s*{(?P<number>.*?)},\s*'
+                             r'pages\s*=\s*{(?P<pages>.*?)},'
+                             r'(?:\s*year\s*=\s*{(?P<year>.*?)},)?'
+                             r'(?:\s*doi\s*=\s*{(?P<doi>.*?)},)?'
+                             r'(?:\s*url\s*=\s*{(?P<url>.*?)},)?',
+                             re.DOTALL)
+            
+    papers = []
+    for match in pattern.finditer(bibtex_content):
+        title = match.group('title').strip()
+        author = match.group('author').strip()
+        journal = match.group('journal').strip()
+        volume = (match.group('volume').strip()
+                  if 'volume' in match.groupdict()
+                  else '')
+        number = match.group('number').strip() if 'number' in match.groupdict() else ''
+        pages = match.group('pages').strip() if 'pages' in match.groupdict() else ''
+        year = match.group('year').strip() if 'year' in match.groupdict() else ''
+        doi = match.group('doi').strip() if ('doi' in match.groupdict() 
+                                             and match.group('doi')) else ''
+        url = match.group('url').strip() if ('url' in match.groupdict() 
+                                             and match.group('url')) else ''
+        papers.append((title, author, journal, volume, number, pages, year, doi, url))
+    
+    print(papers)
     return papers
+
+
+def convert_to_apa_citation(author, title, journal, year, volume, number, pages):
+    citation = (f"{author.strip()} ({year.strip()}). "
+                f"{title.strip()}. "
+                f"{journal.strip()}, "
+                f"{volume.strip()}({number.strip()}), "
+                f"{pages.strip()}.")
+    return citation
+
 
 # Directory containing BibTeX files
 papers_directory = 'papers/'
 
 # List all BibTeX files in the directory
-bibtex_files = [os.path.join(papers_directory, file) for file in os.listdir(papers_directory) if file.endswith('.bib')]
+bibtex_files = [os.path.join(papers_directory, file)
+                for file in os.listdir(papers_directory)
+                if file.endswith('.bib')]
 
 # Extract papers from BibTeX files
 all_papers = []
 for bibtex_file in bibtex_files:
+    print(bibtex_file)
     all_papers.extend(extract_papers_from_bibtex(bibtex_file))
 
 # Sort papers alphabetically by title
-sorted_papers = sorted(all_papers, key=lambda x: x[0].strip())
+sorted_papers = sorted(all_papers, key=lambda x: x[1].strip())
 
 # Read the existing README file
 with open('README.md', 'r') as file:
@@ -35,8 +78,15 @@ existing_papers = readme_content[double_dash_index:]
 
 # Generate the updated list of papers
 updated_readme = introduction + "\n\n"
-for title, _ in sorted_papers:
-    updated_readme += f"- {title.strip()}\n"
+for title, author, journal, year, volume, number, pages, _, _ in sorted_papers:
+    apa_citation = convert_to_apa_citation(author,
+                                           title,
+                                           journal,
+                                           year,
+                                           volume,
+                                           number,
+                                           pages)
+    updated_readme += f"- {apa_citation}\n"
 
 # Write the updated content back to the README file
 with open('README.md', 'w') as file:
