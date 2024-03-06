c41fed930d46d688f822f7a49240a453fcbadeb4
antoineruzette <antoine.ruzette@gmail.com>
2024-03-06 09:48:41 -0500

    Add a couple articles and more robust regex pattern extracting in bibtex files

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 # A collection of classic scientific papers covering biology, engineering and physics.
 
-I read a lot of papers about new and old sciences, spanning biology, engineering, computer science and physics. When it comes to keeping up with the current publications, I enjoy reading the old ones, reading about the years of incredible science that lead to this very moment. Which papers contributed to the development of an entire field? To new ways of thinking? To paradigm shift? 
+I read a lot of scientific papers, recent and older, primarly in biology though my interests span engineering, computer science and physics. When it comes to keeping up with the current publications, I enjoy reading the older ones, learning about the years of incredible science that led to this very moment. Which papers contributed to the development of new fields? To new modes of thinking? To paradigm shifts? 
 
 To those who I find influential, inspirational and visionary, this repository is an attempt at archiving your research. 
 
 --
 
-- Lazebnik, Yuri (2002). Can a biologist fix a radio?--Or, what I learned while studying apoptosis. Cancer cell, 2(3), 179--182.
+- Lazebnik, Yuri. (2002). Can a biologist fix a radio?--Or, what I learned while studying apoptosis. _Cancer cell_, 2(3), 179--182.
+- P. W. Anderson (1972). More Is Different. _Science_, 177(4047), 393-396.
+- Prewitt, Judith M. S. and Mendelsohn, Mortimer L. (1966). THE ANALYSIS OF CELL IMAGES. _Annals of the New York Academy of Sciences_, 128(3), 1035-1053.
+- Schmit, Jeremy and Dill, Ken A. (2023). Next-Gen Biophysics: Look to the Forest, Beyond the Trees. _Annual Review of Biophysics_, 52(1), v-viii.
--- source/extract_bibtex.py
+++ source/extract_bibtex.py
@@ -5,44 +5,52 @@ import re
 def extract_papers_from_bibtex(bibtex_file):
     with open(bibtex_file, 'r') as file:
         bibtex_content = file.read()
-        
-        pattern = re.compile(r'@article{(.*?),\s*'
-                             r'title\s*=\s*{(?P<title>.*?)},\s*'
-                             r'author\s*=\s*{(?P<author>.*?)},\s*'
-                             r'journal\s*=\s*{(?P<journal>.*?)},\s*'
-                             r'volume\s*=\s*{(?P<volume>.*?)},\s*'
-                             r'number\s*=\s*{(?P<number>.*?)},\s*'
-                             r'pages\s*=\s*{(?P<pages>.*?)},'
-                             r'(?:\s*year\s*=\s*{(?P<year>.*?)},)?'
-                             r'(?:\s*doi\s*=\s*{(?P<doi>.*?)},)?'
-                             r'(?:\s*url\s*=\s*{(?P<url>.*?)},)?',
-                             re.DOTALL)
             
+    pattern = re.compile(r'''
+        @article{(?P<id>.*?),
+        \s*title\s*=\s*{(?P<title>.*?)}.*?,
+        \s*author\s*=\s*{(?P<author>.*?)}.*?,
+        \s*journal\s*=\s*{(?P<journal>.*?)}.*?,
+        \s*volume\s*=\s*{(?P<volume>.*?)}.*?,
+        \s*number\s*=\s*{(?P<number>.*?)}.*?,
+        \s*pages\s*=\s*{(?P<pages>.*?)}.*?,
+        \s*year\s*=\s*{(?P<year>.*?)}.*?,
+        \s*doi\s*=\s*{(?P<doi>.*?)}.*?
+        ''', re.DOTALL | re.VERBOSE)
+    
     papers = []
     for match in pattern.finditer(bibtex_content):
+        print(match)
         title = match.group('title').strip()
         author = match.group('author').strip()
         journal = match.group('journal').strip()
         volume = (match.group('volume').strip()
                   if 'volume' in match.groupdict()
                   else '')
-        number = match.group('number').strip() if 'number' in match.groupdict() else ''
-        pages = match.group('pages').strip() if 'pages' in match.groupdict() else ''
-        year = match.group('year').strip() if 'year' in match.groupdict() else ''
-        doi = match.group('doi').strip() if ('doi' in match.groupdict() 
-                                             and match.group('doi')) else ''
-        url = match.group('url').strip() if ('url' in match.groupdict() 
-                                             and match.group('url')) else ''
+        number = (match.group('number').strip() 
+                  if 'number' in match.groupdict() 
+                  else '')
+        pages = (match.group('pages').strip() 
+                 if 'pages' in match.groupdict() 
+                 else '')
+        year = (match.group('year').strip() 
+                if 'year' in match.groupdict() 
+                else '')
+        doi = (match.group('doi').strip() 
+               if 'doi' in match.groupdict() 
+               else '')
+        url = (match.group('url').strip() 
+               if 'url' in match.groupdict() 
+               else '')
         papers.append((title, author, journal, volume, number, pages, year, doi, url))
-    
-    print(papers)
+
     return papers
 
 
 def convert_to_apa_citation(author, title, journal, year, volume, number, pages):
     citation = (f"{author.strip()} ({year.strip()}). "
                 f"{title.strip()}. "
-                f"{journal.strip()}, "
+                f"_{journal.strip()}_, "
                 f"{volume.strip()}({number.strip()}), "
                 f"{pages.strip()}.")
     return citation
@@ -61,6 +69,7 @@ all_papers = []
 for bibtex_file in bibtex_files:
     print(bibtex_file)
     all_papers.extend(extract_papers_from_bibtex(bibtex_file))
+    print(all_papers)
 
 # Sort papers alphabetically by title
 sorted_papers = sorted(all_papers, key=lambda x: x[1].strip())
